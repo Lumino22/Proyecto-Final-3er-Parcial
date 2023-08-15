42d832bc02424b74962e1435707205b1e82bd420
Lumino22 <amiel.amable28@hotmail.com>
2023-08-14 22:37:57 -0400

    Idea_principal

--- src/MatriculacionUniversitaria.c
+++ src/MatriculacionUniversitaria.c
@@ -2,4 +2,21 @@
 
 void main (void){
     //El programa aun esta en proceso de creacion
+
+    int exit;
+
+    switch (exit)
+    {
+    case 1:
+        printf("Ingrese el nombre de Alumno:\n");
+        printf("Ingrese la contrasena de Alumno:\n");
+        break;
+    
+    case 2:
+        printf("Digite el Nombre Completo:\n");
+        printf("Digite el Correo electronico:\n");
+        printf("Digite la Direccion>\n");
+    default:
+        break;
+    }
 }
